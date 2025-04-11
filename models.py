d00124ba9dee3d7e025626ef7a6f0fa264645a95
Andrew Kinyua <andrewkinyua500@gmail.com>
2025-04-12 02:08:51 +0300

    added models info

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class Episode(db.model):
     date = db.Column(db.String)
     number = db.Column(db.Integer)
 
-    apperances= db.relationship("Appearance", backref="episode", cascade="all, delete-orphan")
+    appearances= db.relationship("Appearance", backref="episode", cascade="all, delete-orphan")
 
     def to_dict(self):
         return {
@@ -17,10 +17,48 @@ class Episode(db.model):
             "date": self.date,
             "number": self.number 
             }
-    def to_dict_with_apperances(self):
+    def to_dict_with_appearances(self):
         return {
             "id": self.id, 
             "date": self.date,
             "number": self.number, 
-            "appearances": [apperance.to_dict() for appearance in self.to_dict_with_apperances]
+            "appearances": [appearance.to_dict() for appearance in self.to_dict_with_appearances]
+        }
+    
+class Guest(db.model):
+    __tablename__ = 'guests'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    appearances = db.relationship("Appearance", backref="guests", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return{
+            "id": self.id, 
+            "name": self.name,
+            "occupation": self.occupation
+        }
+    
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+
+    episode_id = db.column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.column(db.Integer, db.ForeignKey('guests.id'))
+
+    @validates('rating')
+    def validate_rating(self, key, value):
+        if not (1<= value <=5):
+            raise ValueError("Rating must be between 1 and 5")
+        return value
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "episode_id": self.episode_id, 
+            "guest_id": self.guest_id,
+            "episode": self.episode.to_dict(),
+            "guest": self.guest.to_dict()
         }
